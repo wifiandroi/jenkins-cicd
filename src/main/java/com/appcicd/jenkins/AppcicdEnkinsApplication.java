73247dbb3936eacce0cd4f2d99cb3821c2239677
unknown <dell@PCP-0638.mtpe.gob.pe>
2026-02-15 18:49:37 -0500

    Primer commit

---
+++ src/main/java/com/appcicd/jenkins/AppcicdEnkinsApplication.java
@@ -0,0 +1,13 @@
+package com.appcicd.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcicdEnkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcicdEnkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appcicd/jenkins/AppcicdEnkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appcicd.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcicdEnkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
